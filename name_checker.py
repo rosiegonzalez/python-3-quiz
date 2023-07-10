c52f78c4ef1918b8bcbfffe4f40d50376a2e80b2
Rosie Gonzalez <rgonzalez@xpanxion.com>
2023-07-10 10:45:14 -0800

    Python Quiz 3

--- name_checker.py
+++ name_checker.py
@@ -1,7 +1,20 @@
 from ValidationException import ValidationException
 
 def validate_file(file_path):
-    pass #TODO: Only add code inside this function.
+    lines = []
+
+    with open(file_path, "r") as file1:
+        while True:
+            line = file1.readline().split()
+
+            if not line:
+                break
+            lines.append(line)
+
+    for i in range(len(lines)):
+        if "1" in lines[i][0]:
+            raise ValidationException(f"Invalid first name: '{lines[i][0]}'")
+
 
 
 def test():
